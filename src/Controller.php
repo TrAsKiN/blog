42e6e246688fa00ad878cd68607fc2d0cd5b2d63
TrAsKiN <marchal.simon@gmail.com>
2022-05-28 13:58:58 +0200

    feat: add Intl twig extension

--- src/Controller.php
+++ src/Controller.php
@@ -3,11 +3,13 @@
 namespace Blog;
 
 use Twig\Environment;
+use Twig\Extra\Intl\IntlExtension;
 
 class Controller
 {
     public function __construct(
         protected Environment $twig
     ) {
+        $this->twig->addExtension(new IntlExtension());
     }
 }
