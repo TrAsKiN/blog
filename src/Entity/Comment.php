a3dd2622d91d5d869746638cc2541962c1a24512
TrAsKiN <marchal.simon@gmail.com>
2022-05-30 21:24:41 +0200

    feat: add entities

---
+++ src/Entity/Comment.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Blog\Entity;
+
+use DateTime;
+
+class Comment
+{
+    public function __construct(
+        private readonly int $id,
+        private string       $content,
+        private DateTime     $createdAt = new DateTime(),
+        private bool         $valid = false
+    ) {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function isValid(): bool
+    {
+        return $this->valid;
+    }
+
+    public function setValid(bool $valid): void
+    {
+        $this->valid = $valid;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Blog\Entity;
+
+class User
+{
+    public function __construct(
+        private readonly int $id,
+        private string $username,
+        private string $email,
+        private string $password,
+        private array $roles = [],
+        private bool $active = false,
+        private ?string $token = null
+    ) {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): void
+    {
+        $this->roles = $roles;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): void
+    {
+        $this->active = $active;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(?string $token): void
+    {
+        $this->token = $token;
+    }
+}
